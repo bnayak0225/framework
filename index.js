0ad35b98d4b7f7a49cad92c6ebd4e805a88e16ae
Biswajit Nayak <bnayak0225@gmail.com>
2021-07-24 19:54:01 +0530

    Head

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 "use strict";
 exports.__esModule = true;
-exports.store = exports.Connect = exports.withRouter = exports.Link = exports.Router = void 0;
+exports.Head = exports.store = exports.Connect = exports.withRouter = exports.Link = exports.Router = void 0;
 var router_1 = require("./router");
 exports.Router = router_1.Router;
 exports.Link = router_1.Link;
 exports.withRouter = router_1.withRouter;
+var Head_1 = require("./Head");
+exports.Head = Head_1.Head;
 var connect_1 = require("./state/connect");
 exports.Connect = connect_1.Connect;
 var store_1 = require("./state/store");
