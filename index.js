5c7de9e468a6ec4c4fe27d88cc613edbf7e62e46
biswa2103 <101873675+biswa2103@users.noreply.github.com>
2022-07-17 15:35:15 +0530

    async fetch api

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 "use strict";
 exports.__esModule = true;
-exports.Head = exports.store = exports.Connect = exports.withRouter = exports.Link = exports.Router = void 0;
+exports.fetchAsyncApi = exports.Head = exports.store = exports.Connect = exports.withRouter = exports.Link = exports.Router = void 0;
 var router_1 = require("./router");
 exports.Router = router_1.Router;
 exports.Link = router_1.Link;
 exports.withRouter = router_1.withRouter;
 var Head_1 = require("./Head");
 exports.Head = Head_1.Head;
+var fetchApi_1 = require("./fetchApi");
+exports.fetchAsyncApi = fetchApi_1.fetchAsyncApi;
 var connect_1 = require("./state/connect");
 exports.Connect = connect_1.Connect;
 var store_1 = require("./state/store");
