c0b21b15a839825326db7ba4673d592daacd34d0
biswa2103 <101873675+biswa2103@users.noreply.github.com>
2022-08-17 21:29:19 +0530

    update

--- state/context.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-var react_1 = __importDefault(require("react"));
-var StoreContext = react_1["default"].createContext({ state: {}, dispatch: function () { } });
-exports["default"] = StoreContext;
--- webpackConfig/config/Plugin.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Plugin = void 0;
-exports.Plugin = {};
